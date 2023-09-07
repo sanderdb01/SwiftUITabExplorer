34ce1da41ab1dec68594240735e6625129b96c41
David Sanders <sanderdb01@gmail.com>
2023-09-07 12:55:20 -0400

    Initial Commit

---
+++ SwiftUITabExplorer/SwiftUITabExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITabExplorerApp.swift
+//  SwiftUITabExplorer
+//
+//  Created by David Sanders on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITabExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
